a247e148ad1aebec4646db4e632914b8f94216f8
VLauren <laurentino.vilchez@gmail.com>
2025-01-25 23:17:51 +0100

    escape para salir

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -26,6 +26,9 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	if(Input.is_action_pressed("ui_cancel")):
+		get_tree().quit()
+		
 	if(dead):
 		return
 		
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -11,8 +11,9 @@ func _process(delta: float) -> void:
 		
 	# if(Input.is_action_just_pressed("ui_accept")):
 	# elif(Input.is_anything_pressed()):
-		
-	if(Input.is_anything_pressed()):
+	if(Input.is_action_pressed("ui_cancel")):
+		get_tree().quit()
+	elif(Input.is_anything_pressed()):
 		get_tree().change_scene_to_file("res://Scenes/levels/level01.tscn")
 	
 # func _unhandled_input(event: InputEvent) -> void:
