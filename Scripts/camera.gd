3f72243d0a11346fdfba0225a13667754cf4749c
VLauren <laurentino.vilchez@gmail.com>
2025-01-25 11:11:27 +0100

    cambio de nivel y mejoras de camara

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -7,6 +7,7 @@ extends Camera3D
 @export var maxPos : Vector2
 
 var player : CharacterBody3D
+var direction = 1
 
 func _ready() -> void:
 	player = get_tree().get_nodes_in_group("player")[0]
@@ -15,7 +16,14 @@ func _process(delta: float) -> void:
 	# print(str(player.position) + " " + str(position))
 	var target = player.position
 	
-	if(not player.is_on_floor()):
+	print((position.y - offset.y) - target.y)
+	if(not player.is_on_floor() and ((position.y - offset.y) - target.y) < -0.5):
 		target.y = position.y - offset.y
+	if player.velocity.x > 0:
+		direction = 1
+	if player.velocity.x < 0:
+		direction = -1
+	
+	target.x += direction * 2.5
 		
 	position = lerp(position, target + offset, smooth_factor * delta)
